d98dd8b97e5c391db11fec189313ff2b6577ca99
Dominika Szaja <dominika.szaja.programista@gmail.com>
2023-08-06 10:59:39 +0200

    Create registration form, save in DB and validate the signup data

---
+++ App/Controllers/Signup.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Models\User;
+
+/**
+ * Signup controller
+
+ */
+class Signup extends \Core\Controller
+{
+  /**
+   * Show the signup page
+   *
+   * @return void
+   */
+  public function newAction()
+  {
+      View::renderTemplate('Signup/new.html');
+  }
+
+  /**
+     * Sign up a new user
+     *
+     * @return void
+     */
+    public function createAction()
+    {
+        $user = new User($_POST);
+
+        if ($user->save()) {
+
+            //View::renderTemplate('Signup/success.html');
+            header('Location: http://' . $_SERVER['HTTP_HOST'] . '/signup/success', true, 303);
+            exit;
+
+        } else {
+
+            View::renderTemplate('Signup/new.html', [
+                'user' => $user
+            ]);
+
+        }
+    }
+
+    /**
+     * Show signup success page
+     * 
+     * @return void
+     */
+    public function successAction()
+    {
+        View::renderTemplate('Signup/success.html'); 
+    }
+}
+
+
